2c427a9cf7666070374b35256a4e8b4ef76eae6b
ESHAN-coder <eshanali9050@gmail.com>
2026-09-25 11:12:09 +0530

    add base files

--- main.py
+++
@@ -1,8 +0,0 @@
-# Read input from the user
-years = int(input("Enter years: "))
-months = int(input("Enter additional months: "))
-
-# Estimate total days (365 days per year, ~30 days per month)x
-total_days = (years * 365) + (months * 30)
-
-print(f"You have lived approximately {total_days} days.")
